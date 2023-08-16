2dd80c99e5d9da0a659cb8c9285a5c2101de4cde
Akash Panchani <akash61p@gmail.com>
2023-08-16 22:37:23 +0530

    First Commit

---
+++ bookstore/src/main/java/com/bookstore/repository/MyBookRepository.java
@@ -0,0 +1,12 @@
+package com.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.entities.MyBookList;
+
+@Repository
+public interface MyBookRepository extends CrudRepository<MyBookList, Integer> {
+
+}
